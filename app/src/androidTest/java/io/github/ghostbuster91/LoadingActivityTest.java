e14dc7d22db687c54f1c2d303aaa32a4e7fd9e7a
ghostbuster91 <kghost0@gmail.com>
2017-11-03 21:26:18 +0100

    Init repo

---
+++ app/src/androidTest/java/io/github/ghostbuster91/LoadingActivityTest.java
@@ -0,0 +1,23 @@
+package io.github.ghostbuster91;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoadingActivityTest {
+
+    @Rule
+    private ActivityTestRule<LoadingActivity> rule = new ActivityTestRule<>(LoadingActivity.class);
+
+    @Test
+    public void assertLoaderIsVisible() {
+        Espresso.onView(ViewMatchers.withId(R.id.progressBar)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+}
---
+++ app/src/main/java/io/github/ghostbuster91/LoadingActivity.java
@@ -0,0 +1,13 @@
+package io.github.ghostbuster91;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
